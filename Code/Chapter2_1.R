ef18e9404453dfb57183c0efc2cf147e57b5930d
omdgit <oliver.dreger@gmail.com>
2018-12-10 23:23:41 -0500

    Added script for problem 2

--- Code/Chapter2_1.R
+++ Code/Chapter2_1.R
@@ -15,3 +15,21 @@ plot(car.ts)
 plot(decompose(air.ts))
 plot(decompose(rail.ts))
 plot(decompose(car.ts))
+
+
+##########################################################
+# Problem 2:
+
+library(readxl)
+DepartmentStoreSales <- read_excel("Data/DepartmentStoreSales.xls", 
+                                   col_types = c("numeric", "numeric"))
+View(DepartmentStoreSales)
+
+sales.ts <- ts(DepartmentStoreSales$Sales, start=c(2000, 1), 
+               end=c(2005, 4), frequency=4)
+sales.ts
+
+plot(sales.ts, type='o')
+abline(v=c(2000:2005), col='grey', lty=2)
+
+plot(decompose(sales.ts), type='o')
